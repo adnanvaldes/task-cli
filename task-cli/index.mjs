71535446e634905e96295a9438623308260961cd
Adnan Valdes <github@terminus.earth>
2024-11-14 23:28:18 -0800

    Add list functionality, along with color

--- task-cli/index.mjs
+++ task-cli/index.mjs
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 
-import Tasks from "./task-cli.mjs"
+import Tasks from "./task-cli.mjs";
 
 const taskCLI = new Tasks();
-taskCLI.markTask(9, "done")
-console.log(taskCLI.taskList)
+taskCLI.listTasks()
--- task-cli/task-cli.mjs
+++ task-cli/task-cli.mjs
@@ -1,122 +1,140 @@
-import { cwd } from 'node:process'
-import fs from 'node:fs'
+import { cwd } from "node:process";
+import fs from "node:fs";
+
+const reset = "\x1b[0m";
+const red = "\x1b[31m";
+const green = "\x1b[32m";
+const yellow = "\x1b[33m";
 
 class Tasks {
-    constructor() {
-        this.taskPath = `${cwd()}/tasks.json`;
-        this.taskList = this.readTasksFile();
-        this.utils = new Utils();
-    };
-    
-    getOrCreateTaskList() {
-        if (fs.existsSync(this.taskPath)) {
-            return this.taskPath;
-        } else {
-            fs.writeFileSync(this.taskPath, "[]");
-        };
+  constructor() {
+    this.taskPath = `${cwd()}/tasks.json`;
+    this.taskList = this.readTasksFile();
+    this.utils = new Utils();
+  }
 
-        return this.taskPath;
-        
-    };
-
-    readTasksFile() {
-        const taskFile = this.getOrCreateTaskList();
-        const taskContent = fs.readFileSync(taskFile);
-        try {
-            return taskContent ? JSON.parse(taskContent) : [];
-        } catch (error) {
-            this.utils.handleErrors(error);
-        }
-        
-    };
-
-    updateTasksFile() {
-        fs.writeFileSync(this.taskPath, JSON.stringify(this.taskList));
+  getOrCreateTaskList() {
+    if (fs.existsSync(this.taskPath)) {
+      return this.taskPath;
+    } else {
+      fs.writeFileSync(this.taskPath, "[]");
     }
 
-    createTask(taskDescription) {
-        const newID = this.utils.getHighestID(this.taskList) + 1
-        const newTask = {
-            id : newID,
-            task: {
-            description : taskDescription,
-            status : "todo",
-            createdAt : new Date().toLocaleString(),
-            updatedAt : null
+    return this.taskPath;
+  }
+
+  readTasksFile() {
+    const taskFile = this.getOrCreateTaskList();
+    const taskContent = fs.readFileSync(taskFile);
+    try {
+      return taskContent ? JSON.parse(taskContent) : [];
+    } catch (error) {
+      this.utils.handleErrors(error);
+    }
+  }
+
+  updateTasksFile() {
+    fs.writeFileSync(this.taskPath, JSON.stringify(this.taskList));
+  }
+
+  createTask(taskDescription) {
+    const newID = this.utils.getHighestID(this.taskList) + 1;
+    const newTask = {
+      id: newID,
+      task: {
+        description: taskDescription,
+        status: "todo",
+        createdAt: new Date().toLocaleString(),
+        updatedAt: null,
+      },
+    };
+    this.taskList.push(newTask);
+    this.updateTasksFile();
+    console.log(`Task added successfully (ID: ${newID})`);
+  }
+
+  updateTask(taskID, taskDescription) {
+    for (let item of this.taskList) {
+      if (taskID == item.id) {
+        item.task.description = taskDescription;
+        item.task.updatedAt = new Date().toLocaleString();
+      }
+      break;
+    }
+    this.updateTasksFile();
+  }
+
+  deleteTask(taskID) {
+    let newTaskList = this.taskList.filter((task) => task.id !== taskID);
+    this.taskList = newTaskList;
+    this.updateTasksFile();
+  }
+
+  markTask(taskID, status) {
+    for (let item of this.taskList) {
+      if (taskID == item.id) {
+        switch (status) {
+          case "todo":
+            item.task.status = "todo";
+            break;
+          case "started":
+            item.task.status = "started";
+            break;
+          case "done":
+            item.task.status = "done";
+            break;
+          default:
+            throw new Error("Mark status is one of [todo | started | done]");
+        }
+        item.task.updatedAt = new Date().toLocaleString();
+      }
+    }
+    this.updateTasksFile();
+  }
+
+  listTasks(status=null) {
+    if (!status) {
+        for (let item of this.taskList) {
+            console.log(`${green}${item.task.description}\n${yellow}Status: ${item.task.status}\n${reset}created ${item.task.createdAt} | updated ${item.task.updatedAt}\n`)
+        }
+    } else {
+        console.log(`${status} tasks:`)
+        for (let item of this.taskList) {
+        if (item.task.status == status) {
+            console.log(`Task: ${green}${item.task.description}\n ${reset}created ${item.task.createdAt} updated ${item.task.updatedAt}\n`)
             }
         }
-        this.taskList.push(newTask);
-        this.updateTasksFile();
-        console.log(`Task added successfully (ID: ${newID})`);
-    };
-
-    updateTask(taskID, taskDescription) {
-        for (let item of this.taskList) {
-            if (taskID == item.id) {
-                item.task.description = taskDescription;
-                item.task.updatedAt = new Date().toLocaleString();
-            };
-            break;
         }
-        this.updateTasksFile();
-    };
-
-    deleteTask(taskID) {
-        let newTaskList = this.taskList.filter(task => task.id !== taskID);
-        this.taskList = newTaskList;
-        this.updateTasksFile();
-    };
-
-    markTask(taskID, status) {
-        for (let item of this.taskList ){
-            if (taskID == item.id) {
-                switch(status){
-                    case "todo":
-                        item.task.status = "todo";
-                        break;
-                    case "started":
-                        item.task.status = "started";
-                        break;
-                    case "done":
-                        item.task.status = "done";
-                        break;
-                    default: throw new Error("Mark status is one of [todo | started | done]")
-                }
-                item.task.updatedAt = new Date().toLocaleString()
-            };
-        };
-        this.updateTasksFile()
-    };
-};
-
+    }
+}
 
 class Utils {
-    getHighestID(taskList) {
-        const isEmptyList = (taskList) => Object.keys(taskList).length == 0;
-        if (isEmptyList(taskList)) {
-            return 0;
-        } else {
-            let currentMax = 0;
-            for (let task of taskList) {
-                if (task.id > currentMax) {
-                    currentMax = task.id;
-                };
-            };
-            return currentMax;
-        };       
-    };
-
-    handleErrors(error) {
-        if (error instanceof SyntaxError) {
-            console.error(`A syntax error occurred: ${error.message}\nCheck your JSON task file?`);
-        } else if (error instanceof TypeError) {
-            console.error("A type error occurred:", error.message);
-        } else {
-            console.error("An error occurred:", error.message);
+  getHighestID(taskList) {
+    const isEmptyList = (taskList) => Object.keys(taskList).length == 0;
+    if (isEmptyList(taskList)) {
+      return 0;
+    } else {
+      let currentMax = 0;
+      for (let task of taskList) {
+        if (task.id > currentMax) {
+          currentMax = task.id;
         }
-    };
+      }
+      return currentMax;
+    }
+  }
+
+  handleErrors(error) {
+    if (error instanceof SyntaxError) {
+      console.error(
+        `A syntax error occurred: ${error.message}\nCheck your JSON task file?`
+      );
+    } else if (error instanceof TypeError) {
+      console.error("A type error occurred:", error.message);
+    } else {
+      console.error("An error occurred:", error.message);
+    }
+  }
 }
 
 export default Tasks;
-
-
