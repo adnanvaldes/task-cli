a3d05a01f4130db363c6878aa152bde435346a10
Adnan Valdes <github@terminus.earth>
2024-11-17 14:05:47 -0800

    improve command-line interface to use an object rather than switch case

--- task-cli/index.mjs
+++ task-cli/index.mjs
@@ -1,45 +1,41 @@
-#!/usr/bin/env node
 
 import Tasks from "./task-cli.mjs";
 import argv from "node:process";
 
 const usage = `
-usage: task-cli [options] [arg1] [arg2]
-  options:
-    add               Add new task to list
-    update            Update task description. arg1 is the task ID to update, arg2 is the new text to insert
-    delete            Deletes a task from list by task ID. arg1 is the ID of the task to be deleted
-    mark-in-progress  Marks task ID=<arg1> as in-progress
-    mark-done         Marks task ID=<arg1> as done
-    list [status]     Lists tasks. Can be filtered by status.     
-`
-
-
+usage: task-cli [command] [arg1] [arg2]
+  commands:
+    add               Add new task. arg1 is the task description.
+    update            Update task description. arg1 is the task ID, arg2 is the new text.
+    delete            Delete a task by task ID. arg1 is the task ID.
+    todo              Mark a task as "to-do". arg1 is the task ID.
+    mark-in-progress  Mark a task as "in-progress". arg1 is the task ID.
+    mark-done         Mark a task as "done". arg1 is the task ID.
+    list [status]     List tasks. Optionally filter by status (todo, in-progress, done).
+`;
 
 const taskCLI = new Tasks();
-const args = argv.argv.slice([2]).map(arg => arg.toLowerCase());
+const args = argv.argv.slice(2).map(arg => arg.toLowerCase());
+
+const commands = {
+  add: () => taskCLI.createTask(args[1]),
+  update: () => taskCLI.updateTask(args[1], args[2]),
+  delete: () => taskCLI.deleteTask(args[1]),
+  todo: () => taskCLI.markTask(args[1], taskCLI.STATUS.TODO),
+  "mark-in-progress": () => taskCLI.markTask(args[1], taskCLI.STATUS.IN_PROGRESS),
+  "mark-done": () => taskCLI.markTask(args[1], taskCLI.STATUS.DONE),
+  list: () => taskCLI.listTasks(args[1]),
+};
 
 
-switch (args[0]) {
-  case "add":
-    taskCLI.createTask(args[1]);
-    break;
-  case "update":
-    taskCLI.updateTask(args[1], args[2]);
-    break;
-  case "delete":
-    taskCLI.deleteTask(args[1]);
-    break;
-  case "mark-in-progress":
-    taskCLI.markTask(args[1], taskCLI.STATUS.IN_PROGRESS);
-    break;
-  case "mark-done":
-    taskCLI.markTask(args[1], taskCLI.STATUS.DONE);
-    break;
-  case "list":
-    taskCLI.listTasks(args[1], args[0]);
-    break;
-  default:
+const command = commands[args[0]];
+if (command) {
+  try {
+    command();
+  } catch (error) {
+    console.error("Error:", error.message);
     console.log(usage);
+  }
+} else {
+  console.log(usage);
 }
-
